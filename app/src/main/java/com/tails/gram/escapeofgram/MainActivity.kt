69d149d8503984be7a4a6065cbddf21560339bbe
Sungyong Son <space010127@gmail.com>
2018-11-08 16:19:53 +0900

    [Init] Project

---
+++ app/src/main/java/com/tails/gram/escapeofgram/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tails.gram.escapeofgram
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class giMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
